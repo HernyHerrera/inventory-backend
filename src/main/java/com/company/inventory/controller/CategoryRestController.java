222a0492c8f0d81164df42057428560ea9337c7c
Herny Hernando Herrera Hernández <hernyherrera@gmail.com>
2025-11-24 23:44:20 +0100

    servicio para actualizar categorias

--- src/main/java/com/company/inventory/controller/CategoryRestController.java
+++ src/main/java/com/company/inventory/controller/CategoryRestController.java
@@ -5,6 +5,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -31,7 +32,8 @@ public class CategoryRestController {
 		return response;
 	}
 	/**
-	 * get categories by id
+	 * get all categories
+	 * @param id
 	 * @return
 	 */
 	@GetMapping("/categories/{id}")
@@ -42,12 +44,25 @@ public class CategoryRestController {
 	}
 	/**
 	 * save categories
+	 * @param category
 	 * @return
 	 */
 	@PostMapping("/categories")
 	public ResponseEntity<CategoryResponseRest> save(@RequestBody Category category){
 
 		ResponseEntity<CategoryResponseRest> response = service.save(category);
+		return response;
+	}
+	/**
+	 * update category by id
+	 * @param category
+	 * @param id
+	 * @return
+	 */
+	@PutMapping("/categories/{id}")
+	public ResponseEntity<CategoryResponseRest> update(@RequestBody Category category, @PathVariable Long id){
+
+		ResponseEntity<CategoryResponseRest> response = service.update(category, id);
 		return response;
 	}
 }
